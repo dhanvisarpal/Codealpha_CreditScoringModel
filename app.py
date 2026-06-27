dc9b8e66c08f960a0f34c3348623f8ee7ed44f8a
dhanvisarpal <dhanvi.sarpal2007@gmail.com>
2026-06-28 00:07:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+st.title("Credit Card Default Prediction")
+
+st.header("Project Overview")
+st.write("This project aims to develop a Credit Scoring Model that predicts whether " \
+"a customer is likely to default on their next credit card payment using historical " \
+"financial and repayment data.The project uses a supervised machine learning approach, " \
+"where multiple classification algorithms are trained and evaluated to identify the most " \
+"suitable model for predicting customer credit risk.")
+
+st.subheader("Features of credit scoring model ")
+st.text("Exploratory Data Analysis (EDA) \n"
+         "Data Cleaning & Preprocessing \n"
+         "Multiple Classification Algorithms\n"
+         "Model Performance Evaluation\n"
+         "Random Forest Model Selection\n"
+         "Credit Risk Prediction \n"
+         "Interactive Streamlit Dashboard")
+
+st.markdown("### Technologies Used")
+st.markdown("- Python")
+st.markdown("- Pandas")
+st.markdown("- NumPy")
+st.markdown("- Scikit-learn")
+st.markdown("- Streamlit")
+
+st.markdown("---")
+st.caption("Credit Card Default Prediction Project | Streamlit + machine learning")
